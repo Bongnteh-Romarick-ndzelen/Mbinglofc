a4a60d09aad85e9b7d8a5f7b9b2490d970632874
Bongnteh-Romarick-ndzelen <124588664+Bongnteh-Romarick-ndzelen@users.noreply.github.com>
2023-07-06 14:40:25 +0100

    django project

---
+++ mbinglofc/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MbinglofcConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mbinglofc'
---
+++ mbinglofc/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+
+# Create your views here.
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+@login_required(login_url='login')
+def signup(request):
+    if request.method =='POST':
+        
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already Exist, used a different email address')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already in used, used a different username')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                
+                #log user in and direct to settings page
+                
+                #create a Profile model for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
+        else:
+            messages.info(request, "Password not Matching")
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
+@login_required(login_url='login')
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        
+        else:
+            messages.info(request, 'Invalid Credentials!')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+def logout(request):
+    pass
