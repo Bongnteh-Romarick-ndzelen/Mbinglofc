a58b14740f5bb90c2f9cb4ab644e63c7d60dc697
Bongnteh-Romarick-ndzelen <124588664+Bongnteh-Romarick-ndzelen@users.noreply.github.com>
2023-08-12 14:08:30 +0100

    updating to push to github

--- mbinglofc/views.py
+++ mbinglofc/views.py
@@ -4,64 +4,79 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
+
 # Create your views here.
-#@login_required(login_url='login')
+@login_required(login_url='login')
 def index(request):
-    return render(request, 'index.html')
-
-
+    return render(request, "index.html")
 
 def signup(request):
-    if request.method =='POST':
-        
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email already Exist, used a different email address')
-                return redirect('signup')
-            
+                messages.info(
+                    request, "Email already Exist, used a different email address"
+                )
+                return redirect("signup")
+
             if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username already in used, used a different username to register')
-                return redirect('signup')
-            
-            
+                messages.info(
+                    request,
+                    "Username already in used, used a different username to register",
+                )
+                return redirect("signup")
+
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
                 user.save()
-                
-                #log user in and direct to settings page
-                
-                #create a Profile model for the new user
+
+                # log user in and direct to settings page
+
+                # create a Profile model for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id
+                )
                 new_profile.save()
-                return redirect('login')
+                return redirect("login")
         else:
-            messages.info(request, "Password not Matching, please enter password again")
-            return redirect('signup')
+            messages.info(
+                request,
+                "Password not Matching, please make sure the two passwords are the same!",
+            )
+            return redirect("signup")
     else:
-        return render(request, 'signup.html')
+        return render(request, "signup.html")
+
+
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
         user = auth.authenticate(username=username, password=password)
-        
+
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
-        
+            return redirect("/")
+
         else:
-            messages.info(request, 'Invalid Credentials!, make sure you enter the correct information in oder to login')
-            return redirect('login')
+            messages.info(
+                request,
+                "Invalid Credentials!, please make sure you enter the correct information in oder to login",
+            )
+            return redirect("login")
     else:
-        return render(request, 'login.html')
+        return render(request, "login.html")
+
+
 def logout(request):
     auth.logout(request)
-    return redirect('login')
-    
+    return redirect("login")
